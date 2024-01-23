dbb192beac09bee6ff535c7b995be22a0c9979e4
Paul Joel <files.pticlavilca@gmail.com>
2024-01-23 12:10:50 -0500

    add files

--- src/main/resources/static/query.sql
+++ src/main/resources/static/query.sql
@@ -1,19 +1,16 @@
-use pos;
-
 insert into category(icon,name)values("checkroom","camisetas");
 insert into category(icon,name)values("checkroom","poleras");
 insert into category(icon,name)values("checkroom","camisas");
 insert into category(icon,name)values("checkroom","pantalones");
 
-
 insert into material_type(name)values("algodon");
-insert into material_Type(name)values("rip");
-insert into material_Type(name)values("jean");
-insert into material_Type(name)values("franela");
-insert into material_Type(name)values("french terry");
-insert into material_Type(name)values("french terry perchado");
-insert into material_Type(name)values("licra");
-insert into material_Type(name)values("suplex");
+insert into material_type(name)values("rip");
+insert into material_type(name)values("jean");
+insert into material_type(name)values("franela");
+insert into material_type(name)values("french terry");
+insert into material_type(name)values("french terry perchado");
+insert into material_type(name)values("licra");
+insert into material_type(name)values("suplex");
 
 insert into enterprise(name)values ("tardos");
 insert into enterprise(name)values("queasila");
