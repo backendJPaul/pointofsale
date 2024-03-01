81bb743b961813562bbcf3f04c9941d28ce53e62
backendJPaul <files.pticlavilca@gmail.com>
2024-02-29 20:10:13 -0500

    add files

--- src/main/java/com/jpaul/controller/CategoryController.java
+++ src/main/java/com/jpaul/controller/CategoryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @CrossOrigin(origins = {"http://127.0.0.1:5500/"})
 @AllArgsConstructor
 @RestController
-@RequestMapping("api/categories")
+@RequestMapping("api/category")
 public class CategoryController{
 
     private ICategoryService iCategoryService;
